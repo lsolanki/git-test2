275052458d84115fa070c868ad4569f43f518036
lsolanki <lsolanki@bestpeers.com>
2018-08-31 16:45:19 +0530

    React Router

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
 import Main from './components/MainComponent';
 
 class App extends Component {
@@ -8,9 +8,11 @@ class App extends Component {
   render() {
     return (
 
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
