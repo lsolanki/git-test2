2105cc314925356b2207a7c419f80365b5a245a2
lsolanki <lsolanki@bestpeers.com>
2019-01-31 19:10:25 +0530

    React Redux Form Validation

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Label, Col, Button, Row } from 'reactstrap';
 
-import { Control, LocalForm } from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
 
@@ -15,7 +22,7 @@ class Contact extends Component {
     handleSubmit(values){
         console.log('Current state is: ' + JSON.stringify(values));
         alert('Current state is: ' + JSON.stringify(values));
-        event.preventDefault();
+        //event.preventDefault();
     }
 
 
@@ -63,7 +70,19 @@ class Contact extends Component {
                                 <Label htmlFor='firstname' md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name='firstname' placeholder='First Name' 
-                                    className="form-control" />
+                                    className="form-control" 
+                                        validators={{ required, maxLength:maxLength(15), minLength:minLength(3) }}
+                                    />
+                                    <Errors 
+                                        className='text-danger'
+                                        model='.firstname'
+                                        show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
 
@@ -71,7 +90,19 @@ class Contact extends Component {
                                 <Label htmlFor='lastname' md={2}>Last Name</Label>
                                 <Col md={10}>
                                     <Control.text model='.lastname' id='lastname' name='lastname' placeholder='Last Name' 
-                                    className="form-control" />
+                                    className="form-control" 
+                                        validators={{ required, maxLength:maxLength(15), minLength:minLength(3) }}
+                                    />
+                                    <Errors 
+                                        className='text-danger'
+                                        model='.lastname'
+                                        show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
 
@@ -79,7 +110,20 @@ class Contact extends Component {
                                 <Label htmlFor='telnum' md={2}>Contact Tel. Number</Label>
                                 <Col md={10}>
                                     <Control.text model='.telnum' id='telnum' name='telnum' placeholder='Tel. No.' 
-                                    className="form-control" />
+                                    className="form-control" 
+                                        validators={{ required, minLength:minLength(10), maxLength:maxLength(10), isNumber }}
+                                    />
+                                    <Errors 
+                                        className='text-danger'
+                                        model='.telnum'
+                                        show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 9 characters',
+                                            maxLength: 'Must be 10 characters',
+                                            isNumber: 'Must be a numbers'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
 
@@ -87,7 +131,18 @@ class Contact extends Component {
                                 <Label htmlFor='email' md={2}>Email</Label>
                                 <Col md={10}>
                                     <Control.text model='.email' id='email' name='email' placeholder='Email' 
-                                    className="form-control" />
+                                    className="form-control"
+                                     validators={{ required, validEmail }}
+                                    />
+                                    <Errors 
+                                        className='text-danger'
+                                        model='.email'
+                                        show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            validEmail: 'Invalid Email Address'
+                                        }}
+                                    />
                                 </Col>
                             </Row>
 
